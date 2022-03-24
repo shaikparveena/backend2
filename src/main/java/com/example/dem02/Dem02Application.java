da14058b4139b3743eee3ac20ef9c83276b7e429
parveena.shaik <parveena.shaik@cotiviti.com>
2022-03-24 17:18:18 +0530

    modified

--- src/main/java/com/example/dem02/Dem02Application.java
+++ src/main/java/com/example/dem02/Dem02Application.java
@@ -1,5 +1,6 @@
 package com.example.dem02;
 
+
 import com.example.dem02.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
